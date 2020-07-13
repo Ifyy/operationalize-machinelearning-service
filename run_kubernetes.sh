bb10a7a53844fd5b0153ff46ad9cc3dcb27dd8bf
Ifeoma Udoye <ifeomaudoye@gmail.com>
2020-07-13 23:14:57 +0000

    Complete task 4 - 7

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ifeomau/ml-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl apply -f kube_deployment.yml
+
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/ml-service 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,11 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ifeomau/ml-api
 
-# Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
 # Push image to a docker repository
+docker push $dockerpath
